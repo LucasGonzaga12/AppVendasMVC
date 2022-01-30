c8c6ddb6e4cb3990800ba3ec8d3aa667bb06d79f
Lucas Gonzaga <lucas.gonzaga12@gmail.com>
2022-01-30 01:56:24 -0300

    Primeiro MVC - Departament

---
+++ SalesWebMVC/Controllers/contato.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Models;
+using System.Collections.Generic;
+
+namespace SalesWebMVC.Controllers
+{
+    public class contato : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> list = new List<Department>();
+            list.Add(new Department { Id = 1, Name = "Eletronicos" });
+            list.Add(new Department { Id = 2, Name = "Roupas" });
+            return View(list);
+        }
+    }
+}
